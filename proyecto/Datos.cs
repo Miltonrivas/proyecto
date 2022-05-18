c1488990303436c138ba4deecd7f1897cae5edbd
Miltonrivas <20220083@santacecilia.edu.sv>
2022-05-17 18:29:55 -0600

    se agregaron msql y la clase datos

---
+++ proyecto/Datos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MySql.Data.MySqlClient;
+using System.Threading.Tasks;
+
+namespace proyecto
+{
+    internal class Datos
+    {
+        public static MySqlConnection ObtenerConexion()
+        {
+            MySqlConnection conectar = new MySqlConnection("server=127.0.0.1; database=rivasdb; Uid=root; pwd =;");
+
+
+
+            conectar.Open();
+            return conectar;
+        }
+    }
+}
